b7ab7ace2b61c5b243164bc7e85ebf2f05a13af7
Tech24cd <tech.cd2024@gmail.com>
2025-09-05 10:07:25 +0200

    Update TechnicienMissionsPage.js

--- src/pages/TechnicienMissionsPage.js
+++ src/pages/TechnicienMissionsPage.js
@@ -12,7 +12,7 @@ const TechnicienMissionsPage = () => {
   useEffect(() => {
     const fetchMissions = async () => {
       try {
-        const response = await fetch("http://localhost:3000/api/missions");
+        const response = await fetch(`${API_BASE_URL}/api/missions`);
         if (!response.ok) throw new Error("Erreur API");
         const allMissions = await response.json();
 
