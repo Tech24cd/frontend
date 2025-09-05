5962dd6c791a7a3a4b53141fa8807fa9e4c5398f
Tech24cd <tech.cd2024@gmail.com>
2025-09-05 10:55:27 +0200

    Update AllMissionsPresta.js

--- src/pages/AllMissionsPresta.js
+++ src/pages/AllMissionsPresta.js
@@ -1,6 +1,8 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import { useEffect, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 // Statuts
 const STATUTS = [
   "Tous",
