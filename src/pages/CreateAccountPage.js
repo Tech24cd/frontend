5c5a77cb37b4b349b043de83c52fe00583982bc9
Tech24cd <tech.cd2024@gmail.com>
2025-09-05 09:32:37 +0200

    Update CreateAccountPage.js

--- src/pages/CreateAccountPage.js
+++ src/pages/CreateAccountPage.js
@@ -24,7 +24,7 @@ const CreateAccountPage = () => {
             return;
         }
         try {
-            const response = await fetch("http://localhost:3000/api/auth/register", {
+            const response = await fetch(`${API_BASE_URL}/api/auth/register", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
