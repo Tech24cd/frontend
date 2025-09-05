dd46688ab335964b013cddd236b29b38022a984a
Tech24cd <tech.cd2024@gmail.com>
2025-09-05 09:37:39 +0200

    Update PlanningPresta.js

--- src/pages/PlanningPresta.js
+++ src/pages/PlanningPresta.js
@@ -44,7 +44,7 @@ const Planning = () => {
     }, []);
     // Puis, fetchMissions, sans dépendance sur updateEvents (car utilisé en conjonction avec useCallback)
     const fetchMissions = useCallback(() => {
-        fetch("http://localhost:3000/api/missions/prestataire", {
+        fetch(`${API_BASE_URL}/api/missions/prestataire", {
             credentials: "include",
         })
             .then((res) => res.json())
