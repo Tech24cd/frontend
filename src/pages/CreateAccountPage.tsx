214f7d6f53699a34b54f9f3b238e0b55bdb8cd83
Tech24cd <tech.cd2024@gmail.com>
2025-09-05 10:15:34 +0200

    Update CreateAccountPage.tsx

--- src/pages/CreateAccountPage.tsx
+++ src/pages/CreateAccountPage.tsx
@@ -31,7 +31,7 @@ const CreateAccountPage = () => {
     }
 
     try {
-      const response = await fetch(`${API_BASE_URL}/api/auth/register", {
+      const response = await fetch(`${API_BASE_URL}/api/auth/register`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
